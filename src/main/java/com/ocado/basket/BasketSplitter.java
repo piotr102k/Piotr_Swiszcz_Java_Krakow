3f184ff5e383860b72e02b97c6760a9d19585aba
piotr102k <piotrswiszcz2002@wp.pl>
2024-04-05 22:21:31 +0200

    reading-config-file

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -1,1 +1,35 @@
-public class a {
+package com.ocado.basket;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import org.json.JSONObject;
+
+public class BasketSplitter {
+    JSONObject jsonConfigFile;
+    public BasketSplitter(String absolutePathToConfigFile){
+        try {
+            Scanner scan = new Scanner(new File(absolutePathToConfigFile));
+            String inputString = new String();
+            while (scan.hasNext()) {
+                inputString+=scan.nextLine();
+            }
+            scan.close();
+            jsonConfigFile=new JSONObject(inputString);
+        } catch(FileNotFoundException e){
+            System.out.println("error 404 file not found");
+        }
+
+    }
+    public Map<String,List<String>> split(List<String> items){
+        Map<String,List<String>> splitResult=new HashMap<>();
+        for (String item:items) {
+            System.out.println(jsonConfigFile.get(item));
+        }
+        return splitResult;
+    }
+}
--- src/main/java/com/ocado/basket/Main.java
+++ src/main/java/com/ocado/basket/Main.java
@@ -1,7 +1,18 @@
-package org.example;
+package com.ocado.basket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String pathToConfig="D:\\Wszystko\\ZadanieJava\\Zadanie\\config.json";
+        BasketSplitter newBasket=new BasketSplitter(pathToConfig);
+        List<String> basket1 = Arrays.asList("Cocoa Butter", "Tart - Raisin And Pecan", "Table Cloth 54x72 White", "Flower - Daisies", "Fond - Chocolate", "Cookies - Englishbay Wht");
+        List<String> basket2 = Arrays.asList("Fond - Chocolate", "Chocolate - Unsweetened", "Nut - Almond, Blanched, Whole", "Haggis", "Mushroom - Porcini Frozen", "Cake - Miini Cheesecake Cherry", "Sauce - Mint", "Longan", "Bag Clear 10 Lb", "Nantucket - Pomegranate Pear", "Puree - Strawberry", "Numi - Assorted Teas", "Apples - Spartan", "Garlic - Peeled", "Cabbage - Nappa", "Bagel - Whole White Sesame", "Tea - Apple Green Tea");
+
+
+
+        newBasket.split(basket1);
     }
 }
